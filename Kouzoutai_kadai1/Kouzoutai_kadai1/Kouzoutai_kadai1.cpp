ccf6a020926dcff13de59fad93051622f0565f7e
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-16 15:57:48 +0900

    課題１

---
+++ Kouzoutai_kadai1/Kouzoutai_kadai1/Kouzoutai_kadai1.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <stdio.h>
+
+// パラメーターの構造体の定義
+struct Parameter {
+	
+	char name[10];
+	int hp;
+	int attack;
+	int defense;
+};
+void DispParameter(Parameter param) {
+	printf("NAME:%s ----------\n", param.name);
+	printf("HP:%d\n", param.hp);
+	printf("攻撃力:%d\n", param.attack);
+	printf("防御力:%d\n", param.defense);
+}
+int main()
+{
+	Parameter Fast_param = {
+	"mike",
+	100,
+	30,
+	10,
+    };
+
+	DispParameter(Fast_param);
+
+	Parameter Secand_param = {
+     "jan",
+	 100,
+	 30,
+	 10,
+	};
+	DispParameter(Secand_param);
+	return 0;
+}
+

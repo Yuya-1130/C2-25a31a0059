36c4cac20de06d9ccc24ccc75fe382b70ead2130
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-16 13:23:20 +0900

    配列　課題１

--- hairetu_kadai1/hairetu_kadai1/hairetu_kadai1.cpp
+++ hairetu_kadai1/hairetu_kadai1/hairetu_kadai1.cpp
@@ -1,8 +1,28 @@
 ﻿
 #include <stdio.h>
+#include <time.h>
+
+//void Ages(int Ages[]) {
+//
+//	printf("年齢は %d です\n",age);
+//}
 
 int main()
 {
-   
+
+	int Ages[] = {
+		15,
+		32,
+		11,
+		48,
+	};
+
+	for (int i = 0; i <= 3; i++) {
+
+		//int ind = ;
+		//int age = Ages[ind];
+		printf("年齢は%dです\n", Ages[i]);
+	}
+	return 0;
 }
 

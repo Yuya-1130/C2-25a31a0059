bcfb4874d685cc50cf3f3ac82d6f02798004813f
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-16 16:26:28 +0900

    課題２途中

---
+++ Koudoutai_kadai2/Koudoutai_kadai2/Koudoutai_kadai2.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <stdio.h>
+// nameとscoreを持つ構造体を定義
+struct Stetas{
+	char name[10];
+	int score;
+};
+//5人分のデータを構造体配列に格納する
+void DispScore(Stetas Charenjer) {
+	printf("名前:%c\n", Charenjer.name);
+	printf("名前:%d\n", Charenjer.score);
+}
+int main()
+{
+	Stetas Charenjer[5] = {
+		
+
+	};
+	return 0;
+}
+
